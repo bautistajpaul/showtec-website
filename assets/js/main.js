98035d5dfe157aaa17205efbc9c6c0aeda233576
John Paul Bautista <117434459+bautistajpaul@users.noreply.github.com>
2026-04-06 21:46:40 +0800

    Add logo to a header and fix navbar and added JS Page Transition

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,23 @@
 console.log('Showtec site loaded');
+
+//--------PAGE TRANSITION
+// Fade IN when page loads
+window.addEventListener("load", () => {
+document.body.classList.add("loaded");
+});
+
+// Fade OUT when clicking links
+document.querySelectorAll("a").forEach(link => {
+if (link.hostname === window.location.hostname) {
+    link.addEventListener("click", function(e) {
+    e.preventDefault();
+    const href = this.href;
+
+    document.body.classList.remove("loaded");
+
+    setTimeout(() => {
+        window.location = href;
+    }, 300); // match CSS duration
+    });
+}
+});
